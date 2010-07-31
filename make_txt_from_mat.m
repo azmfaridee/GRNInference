1c33bb7dd715e91b1fc79a51defb4d430bb2ab8f
Abu Zaher <zaher14@gmail.com>
2010-07-31 13:56:23 +0600

    added matlab script to create text file from matlab data

---
+++ make_txt_from_mat.m
@@ -0,0 +1,10 @@
+X = load('ystnames.mat');
+Y = load('yeast.mat');
+
+fid = fopen('yeast_names.txt', 'wt');
+fprintf(fid, '%c%c%c%c%c%c%c%c%c\n', X.yystr');
+fclose(fid);
+
+fid = fopen('yeast_values.txt', 'wt');
+fprintf(fid, '%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n', Y.Yeast');
+fclose(fid);
