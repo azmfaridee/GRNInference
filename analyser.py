92834bacb5c4562188253761dbaec1293fdea6e6
Abu Zaher Md Faridee <faridee1@umbc.edu>
2016-12-31 15:24:22 -0500

    initial pearson's calculations

--- analyser.py
+++ analyser.py
@@ -1,13 +1,3 @@
-# if __name__ == '__main__':
-#     yeast_names = []
-#     yeast_values = []
-    
-#     with open('yeast_names.txt') as f:
-#         for l in f: yeast_names.append(l.strip('\n\x00'))
-
-#     with open('yeast_values.txt') as f:
-#         for l in f: yeast_values.append(map(lambda x: float(x), l.strip('\n\x00').split('\t')))
-    
 import pandas as pd
 from scipy.io import loadmat
 
@@ -16,7 +6,18 @@ yeastnames_mat = loadmat('ystnames.mat')
 yeast_expressions = pd.DataFrame(yeast_mat['Yeast'])
 yeast_names = pd.DataFrame(yeastnames_mat['yystr'])
 
-df = pd.DataFrame(yeast_mat['Yeast'].T, columns=yeastnames_mat['yystr'])
+df_orig = pd.DataFrame(yeast_mat['Yeast'].T, columns=yeastnames_mat['yystr'])
 
-sample = df.T.sample(100).T
-pearsons_corr = sample.corr()
+# set df as a small sample of the main dataset 
+df = df_orig
+# df = df_orig.T.sample(10).T
+pearsons_corr = df.corr()
+
+corr_threshold = 0.8
+keys = pearsons_corr.keys()
+for i in range(len(keys)):
+    for j in range(i+1, len(keys)):
+        p, q = keys[i], keys[j]
+        # print('Testing out pair ({}, {})'.format(p, q))
+        if pearsons_corr[p][q] > corr_threshold or pearsons_corr[p][q] < -corr_threshold:
+            print('pair ({}, {}) has very high correlations'.format(p, q))
