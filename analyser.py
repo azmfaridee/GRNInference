9f400b502189380d56dd91c2ab19d1ca0df594c8
Abu Zaher Md Faridee <faridee1@umbc.edu>
2016-12-31 14:38:13 -0500

    added code for direct matlab import with pandas and scipy

--- analyser.py
+++ analyser.py
@@ -1,10 +1,18 @@
-if __name__ == '__main__':
-    yeast_names = []
-    yeast_values = []
+# if __name__ == '__main__':
+#     yeast_names = []
+#     yeast_values = []
     
-    with open('yeast_names.txt') as f:
-        for l in f: yeast_names.append(l.strip('\n\x00'))
+#     with open('yeast_names.txt') as f:
+#         for l in f: yeast_names.append(l.strip('\n\x00'))
 
-    with open('yeast_values.txt') as f:
-        for l in f: yeast_values.append(map(lambda x: float(x), l.strip('\n\x00').split('\t')))
+#     with open('yeast_values.txt') as f:
+#         for l in f: yeast_values.append(map(lambda x: float(x), l.strip('\n\x00').split('\t')))
     
+import pandas as pd
+from scipy.io import loadmat
+
+yeast_mat = loadmat('yeast.mat')
+yeastnames_mat = loadmat('ystnames.mat')
+
+yeast_expressions = pd.DataFrame(yeast_mat['Yeast'])
+yeast_names = pd.DataFrame(yeastnames_mat['yystr'])
