b8217590c3a07e1eb73de8244a4eb002ba084e08
Abu Zaher Md Faridee <faridee1@umbc.edu>
2016-12-31 19:43:22 -0500

    latest code

--- analyser.py
+++ analyser.py
@@ -11,7 +11,7 @@ df_orig = pd.DataFrame(yeast_mat['Yeast'].T, columns=yeastnames_mat['yystr'])
 
 # set df as a small sample of the main dataset 
 # df = df_orig
-# df = df_orig.T.sample(10).T
+# df = df_orig.sample(10, axis=1)
 
 # START zero order correlation analysis
 # pearsons_corr = df.corr()
@@ -25,12 +25,14 @@ df_orig = pd.DataFrame(yeast_mat['Yeast'].T, columns=yeastnames_mat['yystr'])
 #             print('pair ({}, {}) has very high correlations'.format(p, q))
 # END zero order correlation analysis
 
-n_samples = 10
+n_samples = 100
+partial_corr_threshold = 0.1
 tripplet_samples =  pd.DataFrame(columns=['a', 'b', 'c', 'r_ac_b'])
 for i in range(n_samples):
     tripplet = df_orig.sample(3, axis=1)
     corr = tripplet.corr()
     r_ab, r_bc, r_ac = corr.iloc[0][1], corr.iloc[1][2], corr.iloc[0][2]
+    # calculate partial correlations
     r_ac_b = (r_ac * r_ab * r_bc) / math.sqrt((1 - math.pow(r_ab, 2)) * (1 - math.pow(r_bc, 2)))
     temp = pd.DataFrame([tripplet.keys().values], columns=['a', 'b', 'c'])
     temp['r_ac_b'] = [[r_ac_b]]
